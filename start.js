40f80272bf46a31694d4ec0ba0c7cb7e73831d1d
Einar W. Høst <einarwh@gmail.com>
2014-03-24 14:10:06 +0100

    new names

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 var repl = require('repl').start({});
 
-repl.context.server = require('./hwserver.js');
-repl.context.game = require('./hwclient.js');
+repl.context.server = require('./server.js');
+repl.context.game = require('./client.js');
 
